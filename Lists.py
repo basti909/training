35999ef605542ee36958437b7e5bf9e03426b603
Bastian Wanke <bastian.wanke@student.fh-kiel.de>
2017-06-06 19:06:47 +0200

    weitere list commands

--- Lists.py
+++ Lists.py
@@ -1,6 +1,24 @@
 print("Ich lerne Python")
 
+# Create List
 a = [1,2,3,4,5,6]
-a.append(input())
+# Append Input-Value
+a.append(int(input()))
 
+print(a)
+
+# Pop List
+a.pop(3)
+print(a)
+
+a.pop()
+print(a)
+
+# Extend List
+b=["hallo","du","da"]
+a.extend(b)
+print(a)
+
+# Remove
+a.remove("du")
 print(a)
